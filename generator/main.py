20759b080200e246b32a54a9f1083d4901a7ce33
kino-ma <ma@kino.ma>
2023-11-14 16:14:34 +0900

    feat: print table

--- generator/main.py
+++ generator/main.py
@@ -1,11 +1,19 @@
 import sys
 
+from format.markdown import Table
 from presenter import yaml
+from presenter.presenter import Presenter
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     presenters = yaml.from_yaml(filename)
 
+    header_row = Presenter.header_row()
+
+    rows = []
     for presenter in presenters:
         row = presenter.to_row()
-        print(row)
+        rows.append(row)
+
+    table = Table(header_row, rows)
+    print(table)
