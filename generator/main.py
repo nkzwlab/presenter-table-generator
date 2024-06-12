f444eb62cdb0becaf713e19e221e3703d5484d98
kino-ma <ma@kino.ma>
2024-06-12 13:37:47 +0900

    feat: support arguments

--- generator/main.py
+++ generator/main.py
@@ -1,10 +1,21 @@
-import sys
+import argparse
 
 from presenter.yaml import from_yaml
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    presenters = from_yaml(filename, randomize=True)
+    parser = argparse.ArgumentParser(
+        description="Generate presenters timetable in Markdown"
+    )
+    parser.add_argument("file", help="YAML file to read from")
+    parser.add_argument(
+        "--no-randomize", help="Switch randomization or not", action="store_true"
+    )
+    args = parser.parse_args()
+
+    filename = args.file
+    randomize = not args.no_randomize
+
+    presenters = from_yaml(filename, randomize=randomize)
     table = presenters.to_table()
 
     print(table)
